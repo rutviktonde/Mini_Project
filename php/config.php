b3b1899be0f46faf3a46be501e29a94450d0f258
Rutvik <rutviktonde349@gmail.com>
2023-12-05 00:41:14 +0530

    Initial Commit

---
+++ php/config.php
@@ -0,0 +1,11 @@
+<?php
+//   $hostname = "localhost";
+//   $username = "root";
+//   $password = "";
+//   $dbname = "chatapp";
+
+  $conn = mysqli_connect("localhost", "root", "", "chat");
+  if($conn){
+    // echo "Database Connected" . mysqli_connect_error();
+  }
+?>
